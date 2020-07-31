f45226cda9ade63d69a73a723dc7f69b012103ff
Murat ALAN <murat.alan@ymail.com>
2020-07-31 13:49:48 +0300

    First Commit

--- GPSMaps/Form1.cs
+++ GPSMaps/Form1.cs
@@ -19,7 +19,7 @@ namespace GPSMaps
 
         private void closeButton_Click(object sender, EventArgs e)
         {
-            this.Close();
+            this.Close(); //Close Window
         }
     }
 }
